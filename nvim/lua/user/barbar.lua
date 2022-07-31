18b15c250d785ac67e4198e0d6d79fa41029dd80
shendypratamaa <shendypratamaa@users.noreply.github.com>
2022-08-01 03:45:34 +0700

    update configuration barbar exclude_ft/name

--- nvim/lua/user/barbar.lua
+++ nvim/lua/user/barbar.lua
@@ -24,8 +24,8 @@ barbar.setup {
   clickable = true,
 
   -- Excludes buffers from the tabline
-  exclude_ft = { "javascript" },
-  exclude_name = { "package.json" },
+  exclude_ft = {},
+  exclude_name = {},
 
   -- Enable/disable icons
   -- if set to 'numbers', will show buffer index in the tabline
