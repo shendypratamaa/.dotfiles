9dfb7014b5bd7c7af7efa9460044aaf4498616d7
shendypratamaa <shendypratamaa@users.noreply.github.com>
2022-08-12 00:46:06 +0700

    update kanagawa themes

--- nvim/lua/user/colorschemelist/kanagawa.lua
+++ nvim/lua/user/colorschemelist/kanagawa.lua
@@ -21,7 +21,7 @@ kanagawa.setup {
   variablebuiltinStyle = { italic = true },
   specialReturn = true, -- special highlight for the return keyword
   specialException = true, -- special highlight for exception handling keywords
-  transparent = true, -- do not set background color
+  transparent = false, -- do not set background color
   dimInactive = false, -- dim inactive window `:h hl-NormalNC`
   globalStatus = false, -- adjust window separators highlight for laststatus=3
   terminalColors = true,
