1f187a2bfb1d9781667a3e5083569235f90def14
shendypratamaa <shendypratamaa@users.noreply.github.com>
2022-08-11 14:41:51 +0700

    add cmp-calc

--- nvim/lua/user/dependency.lua
+++ nvim/lua/user/dependency.lua
@@ -80,6 +80,7 @@ return packer.startup(function(use)
   use "hrsh7th/cmp-nvim-lua"
   use "hrsh7th/cmp-nvim-lsp-signature-help"
   use "hrsh7th/cmp-emoji"
+  use "hrsh7th/cmp-calc"
   use "L3MON4D3/LuaSnip" --snippet engine
   use "rafamadriz/friendly-snippets" -- a bunch of snippets to use
 
