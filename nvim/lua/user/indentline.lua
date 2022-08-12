b68549be2409d5a325b6e41ea282c2ce3d6f0bc9
shendypratamaa <shendypratamaa@users.noreply.github.com>
2022-08-13 00:43:48 +0700

    fix throwing error when lsp attach into buffers

--- nvim/lua/user/indentline.lua
+++ nvim/lua/user/indentline.lua
@@ -5,7 +5,11 @@ if not indent_ok then
 end
 
 vim.opt.list = true
+vim.opt.termguicolors = true
 vim.opt.listchars:append "eol:↴"
 vim.g.indent_blankline_char_list = " "
 
-indent.setup  {}
+indent.setup {
+  char = "",
+  show_trailing_blankline_indent = false,
+}
