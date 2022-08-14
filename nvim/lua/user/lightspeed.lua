d2f984eeab837e246b5ea0eb8e93c8fcfd8eb37c
shendypratamaa <shendypratamaa@users.noreply.github.com>
2022-08-14 15:06:48 +0700

    fix broken lightspeed motion

--- nvim/lua/user/lightspeed.lua
+++ nvim/lua/user/lightspeed.lua
@@ -4,10 +4,13 @@ if not lightspeed_ok then
   return
 end
 
+vim.api.nvim_set_keymap('n', '<leader>j', '<Plug>Lightspeed_s', {})
+vim.api.nvim_set_keymap('n', '<leader>k', '<Plug>Lightspeed_S', {})
+
 lightspeed.setup {
   ignore_case = false,
   exit_after_idle_msecs = { unlabeled = 1000, labeled = nil },
-  --- s/x ---
+  --- s/x --j
   jump_to_unique_chars = { safety_timeout = 400 },
   match_only_the_start_of_same_char_seqs = true,
   force_beacons_into_match_width = false,
