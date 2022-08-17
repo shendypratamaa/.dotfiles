4fa82b9487a6f4750db68c00b0eb34f239613b50
shendypratamaa <shendypratamaa@users.noreply.github.com>
2022-08-17 16:20:08 +0700

    update config mason

--- nvim/lua/user/lsp/mason.lua
+++ nvim/lua/user/lsp/mason.lua
@@ -1,55 +1,58 @@
 local M = {}
 
 function M.setup(lspservers, options, formatter)
-local lspconfig = require 'lspconfig'
+  local lspconfig = require 'lspconfig'
 
--- mason
-require('mason').setup {
-  ui = {
-    border = "none",
-    icons = {
-        package_installed = "✓",
-        package_pending = "➜",
-        package_uninstalled = "✗"
-    }
+  -- mason
+  require('mason').setup {
+    ui = {
+      border = 'rounded',
+      icons = {
+        package_installed = '✓',
+        package_pending = '➜',
+        package_uninstalled = '✗',
+      },
+    },
   }
-}
 
-function listFormat(args)
-  for _ , v in pairs(args) do
-    return v
-  end
-end
-
-require('mason-tool-installer').setup {
-  ensure_installed = { listFormat(formatter) },
-  auto_update = false,
-  run_on_start = true,
-}
-
--- mason-lspconfig
-require('mason-lspconfig').setup {
-  ensure_installed = vim.tbl_keys(lspservers),
-  automatic_installation = false
-}
-
-require('mason-lspconfig').setup_handlers {
-    function(server_name)
-    local opts = vim.tbl_deep_extend("force", options, lspservers[server_name] or {})
-      lspconfig[server_name].setup {opts}
-    end,
-    ["sumneko_lua"] = function()
-      local opts = vim.tbl_deep_extend("force", options, lspservers["sumneko_lua"] or {})
-      lspconfig.sumneko_lua.setup(require("lua-dev").setup {opts})
-    end,
-    ["tsserver"] = function()
-    local opts = vim.tbl_deep_extend("force", options, lspservers["tsserver"] or {})
-    lspconfig.tsserver.setup {
-       disable_commands = false,
-       debug = false,
-       server = opts,
-      }
+  function formatterList(args)
+    for _ , v in pairs(args) do
+      return v
     end
+  end
+
+  require('mason-tool-installer').setup {
+    ensure_installed = { formatterList(formatter) },
+    auto_update = false,
+    run_on_start = true,
+  }
+
+  -- mason-lspconfig
+  require('mason-lspconfig').setup {
+    ensure_installed = vim.tbl_keys(lspservers),
+    automatic_installation = false,
+  }
+
+  require('mason-lspconfig').setup_handlers {
+    function(server_name)
+      local opts =
+        vim.tbl_deep_extend('force', options, lspservers[server_name] or {})
+      lspconfig[server_name].setup { opts }
+    end,
+    ['sumneko_lua'] = function()
+      local opts =
+        vim.tbl_deep_extend('force', options, lspservers['sumneko_lua'] or {})
+      lspconfig.sumneko_lua.setup(require('lua-dev').setup { opts })
+    end,
+    ['tsserver'] = function()
+      local opts =
+        vim.tbl_deep_extend('force', options, lspservers['tsserver'] or {})
+      lspconfig.tsserver.setup {
+        disable_commands = false,
+        debug = false,
+        server = opts,
+      }
+    end,
   }
 end
 
