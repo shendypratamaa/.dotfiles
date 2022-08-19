91242a5e8fc30276117c79f080a2262cad48256e
shendypratamaa <shendypratamaa@users.noreply.github.com>
2022-08-19 22:17:58 +0700

    refactor handlers

--- nvim/lua/user/lsp/handlers.lua
+++ nvim/lua/user/lsp/handlers.lua
@@ -1,21 +1,6 @@
 local M = {}
 
-local lsp_highlight_document = function(client)
-  if client.resolved_capabilities.document_highlight then
-    vim.api.nvim_exec(
-      [[
-        augroup lsp_document_highlight
-          autocmd! * <buffer>
-          autocmd CursorHold <buffer> lua vim.lsp.buf.document_highlight()
-          autocmd CursorMoved <buffer> lua vim.lsp.buf.clear_references()
-        augroup END
-      ]],
-      false
-    )
-  end
-end
-
-function M.setup(client)
+function M.setup()
   local signs = {
     { name = 'DiagnosticSignError', text = '' },
     { name = 'DiagnosticSignWarn', text = '' },
@@ -40,7 +25,6 @@ function M.setup(client)
 
   vim.diagnostic.config(diagnostics_config)
 
-  lsp_highlight_document(client)
 end
 
 return M
