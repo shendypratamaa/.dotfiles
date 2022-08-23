140bc554531f595b0578466e9b5e44682938c315
shendypratamaa <shendypratamaa@users.noreply.github.com>
2022-08-23 20:17:45 +0700

    mason

--- nvim/lua/user/lsp/mason.lua
+++ nvim/lua/user/lsp/mason.lua
@@ -8,6 +8,10 @@ function M.setup(servers, options, formatter)
   local lsp_config = require 'lspconfig'
   local navic = require 'nvim-navic'
 
+  local extend = function(...)
+    vim.tbl_deep_extend(...)
+  end
+
   require('mason').setup {
     ui = {
       border = 'rounded',
@@ -33,14 +37,12 @@ function M.setup(servers, options, formatter)
 
   require('mason-lspconfig').setup_handlers {
     function(server_name)
-      local opts =
-        vim.tbl_deep_extend('force', options, servers[server_name] or {})
+      local opts = extend('force', options, servers[server_name] or {})
       lsp_config[server_name].setup { opts }
     end,
 
     ['sumneko_lua'] = function()
-      local opts =
-        vim.tbl_deep_extend('force', options, servers['sumneko_lua'] or {})
+      local opts = extend('force', options, servers['sumneko_lua'] or {})
       lsp_config.sumneko_lua.setup(require('lua-dev').setup {
         lspconfig = {
           on_attach = function(client, bufnr)
@@ -53,8 +55,7 @@ function M.setup(servers, options, formatter)
     end,
 
     ['tsserver'] = function()
-      local opts =
-        vim.tbl_deep_extend('force', options, servers['tsserver'] or {})
+      local opts = extend('force', options, servers['tsserver'] or {})
       require('typescript').setup {
         disable_commands = false,
         debug = false,
@@ -70,8 +71,7 @@ function M.setup(servers, options, formatter)
     end,
 
     ['jsonls'] = function()
-      local opts =
-        vim.tbl_deep_extend('force', options, servers['jsonls'] or {})
+      local opts = extend('force', options, servers['jsonls'] or {})
       lsp_config.jsonls.setup {
         on_attach = function(client, bufnr)
           client.resolved_capabilities.document_formatting = false
