22be1c0dba40684951d9d40c8ab4857d4e89d01c
shendypratamaa <shendypratamaa@users.noreply.github.com>
2022-08-23 17:04:49 +0700

    update null ls

--- nvim/lua/user/lsp/null_ls.lua
+++ nvim/lua/user/lsp/null_ls.lua
@@ -7,12 +7,14 @@ local formatting = nls.builtins.formatting
 local diagnostics = nls.builtins.diagnostics
 local code_actions = nls.builtins.code_actions
 
+local dotpath = '~/.dotfiles/nvim/'
+
 local sources = {
   -- formatting
   formatting.stylua.with {
     extra_args = {
       '--config-path',
-      vim.fn.expand '~/.dotfiles/nvim/linter-config/stylua.toml',
+      vim.fn.expand(dotpath .. 'stylua.toml'),
     },
   },
   formatting.prettierd.with {
@@ -20,10 +22,12 @@ local sources = {
       PRETTIERD_DEFAULT_CONFIG = vim.fn.expand '~/.prettierrc',
     },
   },
-  formatting.fixjson,
   formatting.markdownlint,
+  formatting.black.with { extra_args = { { '--fast' } } },
+  formatting.isort,
 
   -- diagnostics
+  diagnostics.flake8.with { extra_args = { '--max-line-length=80' } },
   diagnostics.write_good.with { filetypes = { 'markdown', 'text' } },
   diagnostics.eslint_d.with {
     extra_args = {
@@ -31,7 +35,12 @@ local sources = {
       vim.fn.expand '~/.eslintrc',
     },
   },
-  diagnostics.selene,
+  -- diagnostics.selene.with {
+  --   extra_args = {
+  --     '--config',
+  --     vim.fn.expand(dotpath .. 'selene.toml'),
+  --   },
+  -- },
   diagnostics.markdownlint,
 
   -- code-actions
@@ -41,16 +50,12 @@ local sources = {
 
 function M.setup(opts)
   nls.setup {
-    debug = true,
+    -- debug = true,
     debounce = 150,
-    diagnostics_format = '[#{c}] #{m} (#{s})',
+    diagnostics_format = '[#{c}] #{m}',
     sources = sources,
     on_attach = opts.on_attach,
-    -- on_attach = function(client)
-    --   if client.resolved_capabilities.document_formatting then
-    --     vim.cmd 'autocmd BufWritePre <buffer> lua vim.lsp.buf.formatting_sync()'
-    --   end
-    -- end,
+    root_dir = nls_utils.root_pattern '.git',
   }
 end
 
