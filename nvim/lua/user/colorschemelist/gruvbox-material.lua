6383c8d28180dacef31b1b6a3dd8e87246c512a4
shendypratamaa <shendypratamaa@users.noreply.github.com>
2022-09-07 08:17:50 +0700

    update colorscheme config

--- nvim/lua/user/colorschemelist/gruvbox-material.lua
+++ nvim/lua/user/colorschemelist/gruvbox-material.lua
@@ -23,3 +23,49 @@ vim.g.gruvbox_material_diagnostic_virtual_text = 'colored'
 vim.g.gruvbox_material_statusline_style = 'original'
 vim.g.gruvbox_material_palette = 'mix'
 vim.g.gruvbox_material_better_performance = 1
+
+local M = {}
+
+function M.setup()
+  local TelescopePrompt = {
+    TelescopeSelection = {
+      bg = '#3c3836',
+    },
+    TelescopePreviewMatch = {
+      fg = '#a39f83',
+      bg = '#d27e99',
+    },
+    TelescopeMatching = {
+      bg = '#d27222',
+    },
+    TelescopeBorder = {
+      fg = '#282828',
+    },
+    TelescopePromptNormal = {
+      fg = '#ffffff',
+      bg = '#3b3933',
+    },
+    TelescopePromptBorder = {
+      fg = '#3b3933',
+      bg = '#3b3933',
+    },
+    TelescopePromptTitle = {
+      fg = '#f3f3f3',
+      bg = '#957FB8',
+    },
+    TelescopePreviewTitle = {
+      fg = '#f3f3f3',
+      bg = '#D27E99',
+    },
+    TelescopeResultsTitle = {
+      fg = '#f3f3f3',
+      bg = '#FFA066',
+    },
+  }
+
+  for hl, col in pairs(TelescopePrompt) do
+    vim.api.nvim_set_hl(0, hl, col)
+  end
+end
+
+return M
--- nvim/lua/user/colorschemelist/tokyonight.lua
+++ nvim/lua/user/colorschemelist/tokyonight.lua
@@ -1,14 +1,69 @@
--- TOKYONIGHT
-vim.g.tokyonight_terminal_colors = 'xterm-256color'
-vim.g.tokyonight_italic_comments = true -- default
-vim.g.tokyonight_italic_keywords = true -- default
-vim.g.tokyonight_style = 'storm'
-vim.g.tokyonight_terminal_colors = true
-vim.g.tokyonight_transparent = true
-vim.g.tokyonight_hide_inactive_statusline = true
-vim.g.tokyonight_sidebar = {}
-vim.g.tokyonight_transparent_sidebar = true
-vim.g.tokyonight_dark_sidebar = true
-vim.g.tokyonight_dark_float = true
-vim.g.tokyonight_colors = {}
-vim.g.tokyonight_lualine_bold = true
+require('tokyonight').setup {
+  style = 'night', -- The theme comes in three styles, `storm`, a darker variant `night` and `day`
+  transparent = false, -- Enable this to disable setting the background color
+  terminal_colors = true, -- Configure the colors used when opening a `:terminal` in Neovim
+  styles = {
+    -- Style to be applied to different syntax groups
+    -- Value is any valid attr-list value `:help attr-list`
+    comments = 'italic',
+    keywords = 'italic',
+    functions = 'NONE',
+    variables = 'NONE',
+    -- Background styles. Can be "dark", "transparent" or "normal"
+    sidebars = 'dark', -- style for sidebars, see below
+    floats = 'dark', -- style for floating windows
+  },
+  sidebars = { 'qf', 'help' }, -- Set a darker background on sidebar-like windows. For example: `["qf", "vista_kind", "terminal", "packer"]`
+  day_brightness = 0.3, -- Adjusts the brightness of the colors of the **Day** style. Number between 0 and 1, from dull to vibrant colors
+  hide_inactive_statusline = false, -- Enabling this option, will hide inactive statuslines and replace them with a thin border instead. Should work with the standard **StatusLine** and **LuaLine**.
+  dim_inactive = false, -- dims inactive windows
+  lualine_bold = false, -- When `true`, section headers in the lualine theme will be bold
+
+  --- You can override specific color groups to use other groups or a hex color
+  --- fucntion will be called with a ColorScheme table
+  ---@param colors ColorScheme
+  on_colors = function(colors) end,
+
+  --- You can override specific highlights to use other groups or a hex color
+  --- fucntion will be called with a Highlights and ColorScheme table
+  on_highlights = function(hl, c)
+    local prompt = '#2d3149'
+    hl.TelescopeNormal = {
+      bg = c.bg_dark,
+      fg = c.fg_dark,
+    }
+    hl.TelescopeBorder = {
+      bg = c.bg_dark,
+      fg = c.bg_dark,
+    }
+    hl.TelescopePromptNormal = {
+      bg = prompt,
+    }
+    hl.TelescopePromptBorder = {
+      bg = prompt,
+      fg = prompt,
+    }
+    hl.TelescopeSelection = {
+      bg = c.bg_dark,
+    }
+    hl.TelescopePreviewTitle = {
+      fg = '#f3f3f3',
+      bg = '#D27E99',
+    }
+    hl.TelescopePromptTitle = {
+      fg = '#f3f3f3',
+      bg = '#957FB8',
+    }
+    hl.TelescopeResultsTitle = {
+      bg = '#f3f3f3',
+      fg = '#ffa066',
+    }
+    hl.TelescopePreviewMatch = {
+      fg = '#a39f83',
+      bg = '#d27e99',
+    }
+    hl.TelescopeMatching = {
+      bg = '#d27222',
+    }
+  end,
+}
