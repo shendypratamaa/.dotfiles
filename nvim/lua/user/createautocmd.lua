fa52f33a8c7850fd70bdca6da962ea76637f7d3d
shendypratamaa <shendypratamaa@users.noreply.github.com>
2022-09-22 02:40:39 +0700

    remove bufferline status when bufloaded

--- nvim/lua/user/createautocmd.lua
+++ nvim/lua/user/createautocmd.lua
@@ -27,7 +27,6 @@ autocmd({ 'User' }, {
   pattern = { 'AlphaReady' },
   callback = function()
     vim.cmd [[
-      set showtabline=0 | autocmd BufUnload <buffer> set showtabline=2
       set laststatus=0 | autocmd BufUnload <buffer> set laststatus=3
     ]]
   end,
