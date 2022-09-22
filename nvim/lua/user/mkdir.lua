c67ebeda6cdcbf253a2e0ff4a84571f3669ec3c2
shendypratamaa <shendypratamaa@users.noreply.github.com>
2022-09-22 11:11:59 +0700

    add utils for handle creating empty file on directory where directory is not created to

---
+++ nvim/lua/user/mkdir.lua
@@ -0,0 +1,19 @@
+local fn = vim.fn
+
+local mkdir = vim.api.nvim_create_augroup('Mkdir', { clear = true })
+
+vim.api.nvim_create_autocmd({ 'BufWritePre' }, {
+  group = mkdir,
+  callback = function()
+    local dir = fn.expand '<afile>:p:h'
+
+    if dir:find '%l+://' == 1 then
+      return
+    end
+
+    if fn.isdirectory(dir) == 0 then
+      fn.mkdir(dir, 'p')
+      vim.notify(' 🤖 Missing directory was created at : ' .. dir)
+    end
+  end,
+})
