656c97d13e670b230e0f486ad9c3260fe33cec27
shendypratamaa <shendypratamaa@users.noreply.github.com>
2022-09-22 11:12:15 +0700

    update config

--- nvim/lua/user/indentline.lua
+++ nvim/lua/user/indentline.lua
@@ -1,4 +1,5 @@
 local indent_ok, indent = pcall(require, 'indent_blankline')
+local hl = vim.api.nvim_set_hl
 
 if not indent_ok then
   return
@@ -6,10 +7,28 @@ end
 
 vim.opt.list = true
 vim.opt.termguicolors = true
-vim.opt.listchars:append 'eol:↴'
-vim.g.indent_blankline_char_list = ' '
-
-indent.setup {
-  char = '',
-  show_trailing_blankline_indent = false,
+vim.opt.listchars = {
+  eol = '↴',
 }
+
+vim.g.indent_blankline_char_list = { '', '', '', ' ' }
+vim.g.indent_blankline_indent_level = 4
+vim.g.indent_blankline_max_indent_increase = 1
+vim.g.indent_blankline_show_first_indent_level = true
+vim.g.indent_blankline_show_end_of_line = true
+vim.g.indnet_blankline_show_current_context = {
+  '^func',
+  'method',
+  '^if',
+  'for',
+  'arguments',
+  'object',
+  'do_block',
+  'table',
+  'try',
+  'while',
+}
+
+hl(0, 'IndentBlanklineChar', { link = 'LineNr' })
+
+indent.setup()
