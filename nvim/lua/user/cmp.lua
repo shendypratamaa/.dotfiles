48bc40ac983bc52660975d836b744d4e64628941
shendypratamaa <shendypratamaa@users.noreply.github.com>
2022-09-29 01:15:04 +0700

    fix cmp tab

--- nvim/lua/user/cmp.lua
+++ nvim/lua/user/cmp.lua
@@ -1,50 +1,40 @@
-local cmp_status_ok, cmp = pcall(require, 'cmp')
-local context = require 'cmp.config.context'
-local snip_ok, luasnip = pcall(require, 'luasnip')
-local lspkind_ok, lspkind = pcall(require, 'lspkind')
+local cmp_status_ok, cmp = pcall(require, "cmp")
+local context = require "cmp.config.context"
+local snip_ok, luasnip = pcall(require, "luasnip")
+local lspkind_ok, lspkind = pcall(require, "lspkind")
 
 if not cmp_status_ok and snip_ok and lspkind_ok then
   return
 end
 
 local cmp_mapping = cmp.mapping.preset.cmdline()
-local cmp_view = { entries = { name = 'wildmenu', separator = ' | ' } }
+local cmp_view = { entries = { name = "wildmenu", separator = " | " } }
 
-cmp.setup.cmdline(':', {
+cmp.setup.cmdline(":", {
   mapping = cmp_mapping,
   sources = cmp.config.sources({
-    { name = 'path' },
+    { name = "path" },
   }, {
-    { name = 'cmdline' },
+    { name = "cmdline" },
   }),
 })
 
-cmp.setup.cmdline('/', {
+cmp.setup.cmdline("/", {
   view = cmp_view,
   mapping = cmp_mapping,
   sources = cmp.config.sources {
-    { name = 'buffer' },
+    { name = "buffer" },
   },
 })
 
-local has_words_before = function()
-  local line, col = unpack(vim.api.nvim_win_get_cursor(0))
-  return col ~= 0
-    and vim.api
-        .nvim_buf_get_lines(0, line - 1, line, true)[1]
-        :sub(col, col)
-        :match '%s'
-      == nil
-end
-
 cmp.setup {
   enabled = function()
-    local buftype = vim.api.nvim_buf_get_option(0, 'buftype')
-    if buftype == 'prompt' then
+    local buftype = vim.api.nvim_buf_get_option(0, "buftype")
+    if buftype == "prompt" then
       return false
     end
-    return not context.in_treesitter_capture 'comment'
-      and not context.in_syntax_group 'Comment'
+    return not context.in_treesitter_capture "comment"
+      and not context.in_syntax_group "Comment"
   end,
   snippet = {
     expand = function(args)
@@ -60,58 +50,72 @@ cmp.setup {
     native_menu = false,
   },
   mapping = {
-    ['<C-k>'] = cmp.mapping.select_prev_item(),
-    ['<C-j>'] = cmp.mapping.select_next_item(),
-    ['<C-b>'] = cmp.mapping(cmp.mapping.scroll_docs(-4), { 'i', 'c' }),
-    ['<C-f>'] = cmp.mapping(cmp.mapping.scroll_docs(4), { 'i', 'c' }),
-    ['<CR>'] = cmp.mapping.confirm { select = true },
-    ['<C-e>'] = cmp.mapping.abort(),
-    ['<C-Space>'] = cmp.mapping.complete {
+    ["<C-k>"] = cmp.mapping.select_prev_item(),
+    ["<C-j>"] = cmp.mapping.select_next_item(),
+    ["<C-b>"] = cmp.mapping(cmp.mapping.scroll_docs(-4), { "i", "c" }),
+    ["<C-f>"] = cmp.mapping(cmp.mapping.scroll_docs(4), { "i", "c" }),
+    ["<CR>"] = cmp.mapping.confirm { select = true },
+    ["<C-e>"] = cmp.mapping.abort(),
+    ["<C-Space>"] = cmp.mapping.complete {
       config = {
         sources = {
-          { name = 'luasnip' },
+          { name = "luasnip" },
         },
       },
     },
-    ['<Tab>'] = cmp.mapping(function(fallback)
+
+    ["<Tab>"] = function(fallback)
       if cmp.visible() then
         cmp.select_next_item()
       elseif luasnip.expand_or_jumpable() then
-        luasnip.expand_or_jump()
-      elseif has_words_before() then
-        cmp.complete()
+        vim.fn.feedkeys(
+          vim.api.nvim_replace_termcodes(
+            "<Plug>luasnip-expand-or-jump",
+            true,
+            true,
+            true
+          ),
+          ""
+        )
       else
         fallback()
       end
-    end, { 'i', 's' }),
-
-    ['<S-Tab>'] = cmp.mapping(function(fallback)
+    end,
+    ["<S-Tab>"] = function(fallback)
       if cmp.visible() then
         cmp.select_prev_item()
       elseif luasnip.jumpable(-1) then
-        luasnip.jump(-1)
+        vim.fn.feedkeys(
+          vim.api.nvim_replace_termcodes(
+            "<Plug>luasnip-jump-prev",
+            true,
+            true,
+            true
+          ),
+          ""
+        )
       else
         fallback()
       end
-    end, { 'i', 's' }),
+    end,
   },
   formatting = {
     format = lspkind.cmp_format {
-      mode = 'symbol_text',
+      mode = "symbol_text",
       before = function(entry, vim_item)
         vim_item.menu = ({
-          luasnip = '[Snippets]',
-          nvim_lsp = '[LSP]',
-          path = '[Path]',
-          cmdline = '[C]',
-          buffer = '[Buffers]',
-          treesitter = '[Treesitter]',
-          calc = '[Calculation]',
-          emoji = '[Emoji]',
-          neorg = '[Norg]',
+          luasnip = "[Snippets]",
+          nvim_lsp = "[LSP]",
+          path = "[Path]",
+          cmdline = "[C]",
+          buffer = "[Buffers]",
+          treesitter = "[Treesitter]",
+          calc = "[Calculation]",
+          emoji = "[Emoji]",
+          neorg = "[Norg]",
         })[entry.source.name]
 
-        if entry.source.name == 'nvim_lsp' then
+        if entry.source.name == "nvim_lsp" then
           vim_item.dup = 0
         end
 
@@ -120,14 +124,14 @@ cmp.setup {
     },
   },
   sources = cmp.config.sources {
-    { name = 'luasnip' },
-    { name = 'nvim_lsp' },
-    { name = 'emoji' },
-    { name = 'neorg' },
-    { name = 'calc' },
-    { name = 'treesitter' },
+    { name = "luasnip" },
+    { name = "nvim_lsp" },
+    { name = "emoji" },
+    { name = "neorg" },
+    { name = "calc" },
+    { name = "treesitter" },
     {
-      name = 'buffer',
+      name = "buffer",
       keyword_length = 5,
       option = {
         get_bufnrs = function()
@@ -140,7 +144,7 @@ cmp.setup {
       },
     },
     {
-      name = 'path',
+      name = "path",
       option = {
         triling_slash = true,
       },
