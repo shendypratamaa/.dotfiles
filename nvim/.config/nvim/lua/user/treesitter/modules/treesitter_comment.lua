20e4150fb46471aa268455dcac168e3b8a9ba2f6
shendypratamaa <shendypratamaa@gmail.com>
2022-11-25 10:26:14 +0700

    update

--- nvim/.config/nvim/lua/user/treesitter/modules/treesitter_comment.lua
+++ nvim/.config/nvim/lua/user/treesitter/modules/treesitter_comment.lua
@@ -2,7 +2,16 @@ local comment = {
     enable = true,
     enable_autocmd = false,
     config = {
-        lua = "-- %s",
+        lua        = "-- %s",
+        css        = "// %s",
+        json       = "// %s",
+        javascript = {
+            __default     = "// %s",
+            jsx_element   = "{/* %s */}",
+            jsx_fragment  = "{/* %s */}",
+            jsx_attribute = "// %s",
+            comment       = '// %s'
+        },
     },
 }
 
