492b7f80b86069e7c016dd6c718bb7847e2e2b93
shendypratamaa <shendypratamaa@gmail.com>
2022-12-25 07:09:04 +0700

    update

--- sketchybar/.config/sketchybar/controller/disk.sh
+++ sketchybar/.config/sketchybar/controller/disk.sh
@@ -1,9 +1,7 @@
 #!/usr/bin/env sh
 
-# DISK=$(df -H | awk '/\/dev\/disk3s1/ {printf("%s\n", $4*1024/1024)}')
-
-DISK=$(df -H | awk '/\/dev\/disk4s3s1/ {printf("%s\n", $4*1024/1024)}')
+DISK=$(df -H | head -2 | tail -1 | awk '{print $4}')
 
 DISKICON=􀤂
 
-sketchybar -m --set $NAME icon=$DISKICON label="$DISK GB |"
+sketchybar -m --set $NAME icon=$DISKICON label="$DISK |"
